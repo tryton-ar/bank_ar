a05d331f977813dd8609034c7f5991db0cd6d1df
Adrián Bernardi <adrian.bernardi@gmail.com>
2017-07-31 19:13:23 -0300

    Fixes + flake8

--- bank.py
+++ bank.py
@@ -1,16 +1,17 @@
-#This file is part of Tryton.  The COPYRIGHT file at the top level of
-#this repository contains the full copyright notices and license terms.
+# This file is part of the bank_ar module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
+
 from trytond.model import fields
 from trytond.pyson import Eval
 from trytond.pool import PoolMeta
 
-
 __all__ = ['Bank', 'BankAccount']
-__metaclass__ = PoolMeta
 
 
 class Bank:
     __name__ = 'bank'
+    __metaclass__ = PoolMeta
 
     bcra_code = fields.Char('BCRA code')
 
@@ -21,6 +22,8 @@ class Bank:
 
 class BankAccount:
     __name__ = 'bank.account'
+    __metaclass__ = PoolMeta
+
     journal = fields.Many2One('account.journal', 'Account Journal',
         required=True, states={'readonly': ~Eval('active', True)},
         depends=['active'])
