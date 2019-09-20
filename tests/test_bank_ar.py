8e37922c2875ecd682fd93f383d5a4a3852c27ea
Luciano Rossi <lukio@gcoop.coop>
2019-09-20 14:58:06 -0300

    Update tests

---
+++ tests/test_bank_ar.py
@@ -0,0 +1,66 @@
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
+import unittest
+import trytond.tests.test_tryton
+from trytond.tests.test_tryton import ModuleTestCase, with_transaction
+from trytond.pool import Pool
+
+
+class BankArTestCase(ModuleTestCase):
+    'Test BankAr module'
+    module = 'bank_ar'
+
+    @with_transaction()
+    def test_cbu_format(self):
+        'Test CBU format'
+        pool = Pool()
+        Party = pool.get('party.party')
+        Bank = pool.get('bank')
+        Account = pool.get('bank.account')
+        Number = pool.get('bank.account.number')
+
+        party = Party(name='Test')
+        party.save()
+        bank = Bank(party=party)
+        bank.save()
+        account, = Account.create([{
+                    'bank': bank.id,
+                    'numbers': [('create', [{
+                                    'type': 'cbu',
+                                    'number': '2850590940090418135201'
+                                    }, {
+                                    'type': 'other',
+                                    'number': 'not CBU',
+                                    }])],
+                    }])
+
+        cbu_number, other_number = account.numbers
+        self.assertEqual(cbu_number.type, 'cbu')
+        self.assertEqual(other_number.type, 'other')
+
+        # Test format on create
+        self.assertEqual(cbu_number.number, '28505909 40090418135201')
+        self.assertEqual(other_number.number, 'not CBU')
+
+        # Test format on write
+        cbu_number.number = '2850590940090418135201'
+        cbu_number.type = 'cbu'
+        cbu_number.save()
+        self.assertEqual(cbu_number.number, '28505909 40090418135201')
+
+        other_number.number = 'still not CBU'
+        other_number.save()
+        self.assertEqual(other_number.number, 'still not CBU')
+
+        Number.write([cbu_number, other_number], {
+                'number': '2850590940090418135201',
+                })
+        self.assertEqual(cbu_number.number, '28505909 40090418135201')
+        self.assertEqual(other_number.number, '2850590940090418135201')
+
+
+def suite():
+    suite = trytond.tests.test_tryton.suite()
+    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
+            BankArTestCase))
+    return suite
